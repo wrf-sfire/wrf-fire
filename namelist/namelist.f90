d72800dd964126e65a031c7bb20533dc49ed1c80
Jan Mandel <jan.mandel@gmail.com>
2019-11-27 01:11:16 -0700

    oputput namelist too

--- namelist/namelist.f90
+++ namelist/namelist.f90
@@ -1,10 +1,13 @@
 real::a(3,4), b(10), c(3)
 integer::ierr,iounit,i
-namelist /na/ a
+character(len=40), dimension(100)::x
+data x /100*'1'/
+namelist /na/ a,x
 namelist /nb/ b
 namelist /nc/ c
 a=0
-OPEN(iounit, FILE='namelist.test',FORM='FORMATTED',STATUS='OLD',ACTION='READ',IOSTAT=ierr)
+iounit = 11
+OPEN(iounit, FILE='namelist.test',FORM='FORMATTED',STATUS='OLD',ACTION='READ')
 read(iounit,nb,end=10)
 go to 11
 10 print *,'namelist nb not found'
@@ -14,4 +17,12 @@ read(iounit,na)
 do i=1,size(a,1)
   print *,(a(i,j),j=1,size(a,2))
 enddo
+print *,a
+close(iounit)
+OPEN(iounit, FILE='namelist.out',FORM='FORMATTED',STATUS='UNKNOWN',ACTION='WRITE')
+write(iounit,na)
+close(iounit)
+OPEN(iounit, FILE='namelist.out',FORM='FORMATTED',STATUS='OLD',ACTION='READ')
+read(iounit,na)
+close(iounit)
 end
