fb1acc111831c463eac753e66cb45478a0aefcb3
Jan Mandel <jan.mandel@gmail.com>
2019-11-26 22:41:45 -0700

    skipping namelist

--- namelist/namelist.f90
+++ namelist/namelist.f90
@@ -1,8 +1,15 @@
-real::a(3,4)
+real::a(3,4), b(10), c(3)
 integer::ierr,iounit,i
 namelist /na/ a
+namelist /nb/ b
+namelist /nc/ c
 a=0
 OPEN(iounit, FILE='namelist.test',FORM='FORMATTED',STATUS='OLD',ACTION='READ',IOSTAT=ierr)
+read(iounit,nb,end=10)
+go to 11
+10 print *,'namelist nb not found'
+rewind(iounit)
+11 continue
 read(iounit,na)
 do i=1,size(a,1)
   print *,(a(i,j),j=1,size(a,2))
