6cee4400a60444e3173bec5997be11d08dadb0a4
Jan Mandel <jan.mandel@gmail.com>
2020-05-12 19:51:03 -0400

    bench fix

--- test/em_fire/bench.sh
+++ test/em_fire/bench.sh
@@ -5,11 +5,12 @@ for i in 1 2 3 4 5 6 7 8 9 10
 do
 declare d=hill-${i}x${i}
 declare np=`echo "$i*$i" | bc`
-declare sz=`echo "42*$i" | bc`
-echo $i $d $np $sz
+declare sz1=`echo "42*$i" | bc`
+declare sz2=`echo "42*$i" | bc`
+echo $i $d $np $sz1 $sz2
 rm -rf $d
 cp -a hill-xxx $d
-sed "s/SZ/$sz/" < hill-xxx/namelist.input > $d/namelist.input
+cat hill-xxx/namelist.input | sed "s/SZ1/$sz2/" | sed "s/SZ2/$sz2/"  > $d/namelist.input
 sed "s/NP/$np/" < hill-xxx/run.sh > $d/run.sh
 $d/run.sh
 done
--- test/em_fire/hill-xxx/run.sh
+++ test/em_fire/hill-xxx/run.sh
@@ -3,6 +3,9 @@ cd $(dirname "$0")
 
 mpirun -np 1 ./ideal.exe
 
-time mpirun -np NP ./wrf.exe
+mv rsl.error.0000 ideal.log
+
+/bin/time -v mpirun -np NP ./wrf.exe |& cat 
+
 ls -lh wrfout*
 rm -f wrfout* rsl.out.*
