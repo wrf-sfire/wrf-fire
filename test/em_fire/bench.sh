63556c7f87cdbfc7b07dd1e0de58ecf83a3d8d1b
Jan Mandel <jan.mandel@gmail.com>
2020-05-14 12:01:04 -0600

    better batch case on Cheyenne

--- test/em_fire/bench.sh
+++ test/em_fire/bench.sh
@@ -3,18 +3,20 @@ cd $(dirname "$0")
 
 for i in 1 2 3 4 5 6 7 8 9 10
 do
-declare d=hill-${i}x${i}
 declare np=`echo "$i*$i" | bc`
-declare sz1=`echo "42*$i" | bc`
-declare sz2=`echo "42*$i" | bc`
+declare patch=42
+declare nm=hill_$patch-${i}x${i}
+declare sz1=`echo "$patch*$i" | bc`
+declare sz2=`echo "$patch*$i" | bc`
 declare ppn=36
 declare nd=`echo "($np+$ppn-1)/$ppn" | bc`
-echo $i $d $np $sz1 $sz2
+echo $i $nm $np $sz1 $sz2
+declare d=$nm
 rm -rf $d
 cp -a hill-xxx $d
 cat hill-xxx/namelist.input | sed "s/SZ1/$sz2/" | sed "s/SZ2/$sz2/"  > $d/namelist.input
-sed "s/NP/$np/" < hill-xxx/run.sh > $d/run.sh
-sed "s/NP/$np/" < hill-xxx/run.pbs | sed "s/ND/$nd/" | sed "s/ND/$nd/" > $d/run.pbs
+sed "s/NP/$np/" < hill-xxx/run.sh | sed "s/NM/$nm/" > $d/run.sh
+sed "s/NP/$np/" < hill-xxx/run.pbs | sed "s/ND/$nd/" | sed "s/NM/$nm/" > $d/run.pbs
 # $d/run.sh
 (cd $d; qsub run.pbs; cd ..)
 done
